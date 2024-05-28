7ff42d07033b9048708a58d4961686160deb02ac
Tanisha Jain <tanishajn12@gmail.com>
2024-05-28 21:17:18 +0530

    New codes

--- BackendLec-4/templating/app.js
+++ BackendLec-4/templating/app.js
@@ -6,25 +6,26 @@ const path = require("path");
 app.set("views" , path.join(__dirname , 'views'));  //better way
 app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.get('/', (req,res)=> {
+//     // res.send("hello all");
+//     let rn = Math.floor(Math.random()*24+5)
+//     res.render("index",{rn}); 
+//     // rn will be sent as object
+
+// });
+let rn = Math.floor(Math.random()*(25)+6)
 
 app.get('/', (req,res)=> {
-    // res.send("hello all");
-    let rn = Math.floor(Math.random()*24+5)
     res.render("index",{rn}); 
-    // rn will be sent as object
-
 });
 
-let todos=["shopping" , "study at 10"];
+// let todos=["grocery shopping" , "assignment maths", "dsa contest"];
 
-app.get("/product", (req,res)=> {
-    res.render("product",{todos});
-});
-
-
-
-
+// app.get("/product", (req,res)=> {
+//     res.render("product",{todos});
+// });
 
 
 app.listen(8080,function() {
