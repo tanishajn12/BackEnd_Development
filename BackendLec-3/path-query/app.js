f869477767aa40b668a9892c69c39df4a0aa7f4b
Tanisha Jain <tanishajn12@gmail.com>
2024-05-28 17:49:41 +0530

    New changes made

--- BackendLec-3/path-query/app.js
+++ BackendLec-3/path-query/app.js
@@ -1,17 +1,24 @@
 const express= require('express');
 const app= express();
 
+// app.get('/r/cat', (req,res)=> {
+//     res.send('cat');
+// })
 
-app.get("/r/:subreddit", (req,res) => {
-    //console.log(req.params)
-    let {subreddit} = req.params;
-    res.send(subreddit);
-});
+// app.get('/r/dog', (req,res)=> {
+//     res.send('dog');
+// })
+
+// app.get("/r/:subreddit", (req,res) => {
+//     console.log(req.params)
+//     let {subreddit} = req.params;
+//     res.send(subreddit);
+// });
 
 app.get("/search",(req,res)=>{
     console.log(req.query);
-    let {firstName, lastName, mentor } = req.query;
-    res.send({firstName, lastName, mentor});
+    // let {firstName, lastName, mentor } = req.query;
+    res.send("req sent to console");
 });
 
 
