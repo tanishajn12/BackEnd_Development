0ea4381958fc3126aac9f19c92539642703d3ade
Tanisha Jain <tanishajn12@gmail.com>
2024-05-31 20:50:30 +0530

    New codes added

---
+++ postman/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+//middleware used otherwise data would be undefined
+app.use(express.json()) //for parsing json
+
+app.get('/', (req,res)=>{
+    res.send("hi welcome to postman")
+})
+
+app.post('/', (req,res)=> {
+    const data = req.body;
+    console.log('data',data);
+    res.send("posting on server")
+})
+let PORT = 8080;
+app.listen(PORT, ()=> {
+    console.log(`server connected at port ${PORT}`)
+})
